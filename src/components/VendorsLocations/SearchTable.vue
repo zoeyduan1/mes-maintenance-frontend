d85d1aee77f2f77366ff901ce593918951b5a06e
zoeyduan1 <zoey.duan@fpscorp.ca>
2025-07-16 11:53:13 -0700

    no pagination

--- src/components/VendorsLocations/SearchTable.vue
+++ src/components/VendorsLocations/SearchTable.vue
@@ -1,28 +1,20 @@
 <template>
-  <div>
-    <el-table height="240" :data="paginatedData" style="width: 100%">
-      <el-table-column label="Name" prop="name" />
-      <el-table-column label="Code" prop="code" />
-      <el-table-column label="Serial" prop="serial_number" />
-      <el-table-column label="Group" prop="equipment_group" />
-      <el-table-column label="Vendor" prop="vendor" />
-      <el-table-column align="right">
-        <template #header>
-          <el-input v-model="search" size="medium" placeholder="Search by name" />
-        </template>
-      </el-table-column>
-    </el-table>
-
-    <div class="mt-4 flex justify-end">
-      <el-pagination
-        layout="prev, pager, next"
-        :page-size="pageSize"
-        :current-page="currentPage"
-        :total="filteredEquipment.length"
-        @current-change="handlePageChange"
-      />
-    </div>
-  </div>
+  <el-table
+    height="240"
+    :data="filteredEquipment"
+    style="width: 100%"
+  >
+    <el-table-column label="Name" prop="name" />
+    <el-table-column label="Code" prop="code" />
+    <el-table-column label="Serial" prop="serial_number" />
+    <el-table-column label="Group" prop="equipment_group" />
+    <el-table-column label="Vendor" prop="vendor" />
+    <el-table-column align="right">
+      <template #header>
+        <el-input v-model="search" size="default" placeholder="Search by name" />
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup>
@@ -36,19 +28,8 @@ const props = defineProps( {
 } )
 
 const search = ref( '' )
-const currentPage = ref( 1 )
-const pageSize = ref( 5 )
 
 const filteredEquipment = computed( () =>
   props.equipmentList.filter( e => !search.value || e.name.toLowerCase().includes( search.value.toLowerCase() ) )
 )
-
-const paginatedData = computed( () => {
-  const start = ( currentPage.value - 1 ) * pageSize.value
-  return filteredEquipment.value.slice( start, start + pageSize.value )
-} )
-
-const handlePageChange = page => {
-  currentPage.value = page
-}
 </script>
