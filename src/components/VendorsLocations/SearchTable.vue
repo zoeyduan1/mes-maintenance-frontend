3270944895530f991d9cf323f442bf829afa2d23
zoeyduan1 <zoey.duan@fpscorp.ca>
2025-07-16 13:53:24 -0700

    fields

--- src/components/VendorsLocations/SearchTable.vue
+++ src/components/VendorsLocations/SearchTable.vue
@@ -2,9 +2,7 @@
   <el-table height="240" :data="filteredEquipment" style="width: 100%">
     <el-table-column label="Name" prop="name" />
     <el-table-column label="Code" prop="code" />
-    <el-table-column label="Serial" prop="serial_number" />
     <el-table-column label="Group" prop="equipment_group" />
-    <el-table-column label="Vendor" prop="vendor" />
     <el-table-column align="right">
       <template #header>
         <el-input v-model="search" size="default" placeholder="Search" />
